d949f95efc8dc156c11b03b1d4fdadf9fb4be4bd
PensActius <37923848+pensactius@users.noreply.github.com>
2018-10-06 12:12:05 +0200

    Add files via upload

---
+++ MeArm_TestBase.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+const uint8_t BASE_PIN    = 5;
+const uint8_t ANGLE_START = 90;
+
+Servo base;
+
+void setup() {
+  base.attach (BASE_PIN);
+  base.write (ANGLE_START);
+}
+
+void loop() {
+//  for (uint8_t ang = ANGLE_START-50; ang != ANGLE_START+50; ++ang) {
+//    base.write (ang);
+//    delay (10);
+//  }
+//  for (uint8_t ang = ANGLE_START+50; ang != ANGLE_START-50; --ang) {
+//    base.write (ang);
+//    delay (10);
+//  }
+}
